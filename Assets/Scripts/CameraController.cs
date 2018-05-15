2d4b94e13cadc364722d089712b1f3fcd164cece
ivanpisarenko <39300716+ivanpisarenko@users.noreply.github.com>
2018-05-15 15:07:27 +0300

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    // Сдвиг камеры относительно позиции объекта
+    private Vector3 offset;
+    // Объект, за которым будет следить камера
+    public GameObject target;
+    // Use this for initialization
+    void Start() {
+        offset = target.transform.position - transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void Update() { 
+
+		// Обновляем положение камеры
+        transform.position = target.transform.position - offset;
+	}
+}
